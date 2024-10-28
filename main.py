0b7782ad84d59ce9c76fe9e9f576806c3081dff9
Zuhair <rawootzuhairahmed@gmail.com>
2024-10-28 23:42:32 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service('chromedriver-win64/chromedriver.exe')
-driver = webdriver.chrome(service)
+service = Service('chromedriver-win64/chromedriver')
+driver = webdriver.Chrome(service=service)
+driver.get('https://demoqa.com/login')
 
+input("Press Enter to exit the browser")
+driver.quit()
