976605b9a4beaea9f47f8494a752972ad06bd042
Zuhair <rawootzuhairahmed@gmail.com>
2024-11-01 20:04:14 +0530

    comments

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+# Set up Chrome options and driver service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
 service = Service('chromedriver-win64/chromedriver.exe')
 driver = webdriver.Chrome(options=chrome_options, service=service)
+
+# Open the target login page
 driver.get('https://demoqa.com/login')
 
+# Wait until the username, password fields, and login button are visible
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
 login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'login')))
 
+# Enter login credentials and click login
 username_field.send_keys('yourusername')
-password_field.send_keys('yourpaswword')
+password_field.send_keys('yourpassword')
 login_button.click()
 
-
+# Wait for user input to exit
 input("Press Enter to exit the browser")
 driver.quit()
